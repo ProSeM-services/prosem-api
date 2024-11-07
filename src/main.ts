19d92f52ea1bb2e4ffc5715ec6646602cecb620c
Francisco-Villanueva <franciscovillanuevaj99@gmail.com>
2024-11-07 20:23:48 -0300

    feat (docs): add swagger documentation api

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { ValidationPipe } from '@nestjs/common'
 import { CORS } from './core/constants'
 import './core/config/env.config'
 import { ZodValidationPipe } from '@anatine/zod-nestjs'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
 	app.enableCors(CORS)
@@ -19,6 +21,17 @@ async function bootstrap() {
 		})
 	)
 	app.useGlobalPipes(new ZodValidationPipe())
+
+	const config = new DocumentBuilder()
+		.setTitle('ProSeM API')
+		.setDescription('API documentation for ProSeM')
+		.setVersion('1.0')
+		.addBearerAuth()
+		.build()
+
+	const document = SwaggerModule.createDocument(app, config)
+	SwaggerModule.setup('docs', app, document)
+
 	await app.listen(process.env.PORT)
 	console.log(
 		`ProSeM | Server Running at ${process.env.PORT} \n Enviroment ===>  ${process.env.NODE_ENV}`
